f2907ee5ef89d253240099ce225ee5039df3b5f0
Friedrich Weinmann <FriedrichWeinmann@users.noreply.github.com>
2024-06-17 11:18:52 +0200

    update

---
+++ functions/general/Disable-ParameterCompletion.ps1
@@ -0,0 +1,51 @@
+﻿function Disable-ParameterCompletion {
+	<#
+	.SYNOPSIS
+		Prevents a particular parameter from being part of the tab completion.
+	
+	.DESCRIPTION
+		Prevents a particular parameter from being part of the tab completion.
+		Note: On PS7 this also prevents tab completion of common parameters.
+	
+	.PARAMETER Command
+		The command to update the parameter for.
+		Can be either a full name or the actual command object.
+	
+	.PARAMETER ParameterName
+		The name of the parameter to hide.
+		Does not support wildcards.
+	
+	.EXAMPLE
+		PS C:\> Disable-ParameterCompletion -Command Get-ChildItem -ParameterName Path
+
+		Hides the "-Path" parameter on Get-ChildItem.
+	#>
+	[CmdletBinding()]
+	param (
+		[Parameter(Mandatory = $true, ValueFromPipeline = $true)]
+		$Command,
+
+		[Parameter(Mandatory = $true)]
+		[string]
+		$ParameterName
+	)
+
+	process {
+		foreach ($commandItem in $Command) {
+			$commandObject = $commandItem
+			if ($commandItem -is [string]) { $commandObject = Get-Command -Name $commandItem }
+
+			if ($commandObject.Parameters.Keys -notcontains $ParameterName) {
+				Write-Error "Command $($commandObject.Name) does not contain parameter $($ParameterName)"
+				continue
+			}
+
+			foreach ($attribute in $commandObject.Parameters.$ParameterName.Attributes) {
+				if (-not ($attribute -is [System.Management.Automation.ParameterAttribute])) {
+					continue
+				}
+				$attribute.DontShow = $true
+			}
+		}
+	}
+}
---
+++ functions/general/Enable-ParameterCompletion.ps1
@@ -0,0 +1,51 @@
+﻿function Enable-ParameterCompletion {
+	<#
+	.SYNOPSIS
+		Makes a parameter (that must exist) available for tab completion when it originally was not.
+	
+	.DESCRIPTION
+		Makes a parameter (that must exist) available for tab completion when it originally was not.
+		This counteracts the "DontShow" parameter attribute.
+	
+	.PARAMETER Command
+		The command to update the parameter for.
+		Can be either a full name or the actual command object.
+	
+	.PARAMETER ParameterName
+		The name of the parameter to show.
+		Does not support wildcards.
+	
+	.EXAMPLE
+		PS C:\> Enable-ParameterCompletion -Command Export-Csv -ParameterName NoTypeInformation
+
+		Tries to enable the NoTypeInformation parameter for tab completion
+	#>
+	[CmdletBinding()]
+	param (
+		[Parameter(Mandatory = $true, ValueFromPipeline = $true)]
+		$Command,
+
+		[Parameter(Mandatory = $true)]
+		[string]
+		$ParameterName
+	)
+
+	process {
+		foreach ($commandItem in $Command) {
+			$commandObject = $commandItem
+			if ($commandItem -is [string]) { $commandObject = Get-Command -Name $commandItem }
+
+			if ($commandObject.Parameters.Keys -notcontains $ParameterName) {
+				Write-Error "Command $($commandObject.Name) does not contain parameter $($ParameterName)"
+				continue
+			}
+
+			foreach ($attribute in $commandObject.Parameters.$ParameterName.Attributes) {
+				if (-not ($attribute -is [System.Management.Automation.ParameterAttribute])) {
+					continue
+				}
+				$attribute.DontShow = $false
+			}
+		}
+	}
+}
