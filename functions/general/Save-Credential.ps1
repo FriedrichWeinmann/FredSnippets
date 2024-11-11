92150d05c194e32d6df7ec74e4dcd7659d85789b
Friedrich Weinmann <FriedrichWeinmann@users.noreply.github.com>
2024-11-11 10:46:03 +0100

    Update Save-Credential.ps1

--- functions/general/Save-Credential.ps1
+++ functions/general/Save-Credential.ps1
@@ -1,6 +1,5 @@
-function Save-Credential
-{
-	<#
+function Save-Credential {
+    <#
 		.SYNOPSIS
 			Stores credentials securely for use by the specified account.
 
@@ -20,9 +19,14 @@ function Save-Credential
 			Defaults to the system account.
 			Offer a full credentials object for a regular user account.
 
+		.PARAMETER AccessGMSA
+			The gMSA that should be able to access the credentials.
+			Make sure the target computers are allowed to retrieve the password for the gMSA!
+			Provide the full NTAccount name. Example: contoso\svcMyName$
+
 		.PARAMETER ComputerName
 			The computer(s) to write the credential to.
-	
+
 		.PARAMETER Credential
 			The credentials to use to authenticate to the target system.
 			NOT the credentials stored for reuse.
@@ -31,147 +35,150 @@ function Save-Credential
 			PS C:\> Save-Credential -ComputerName DC1,DC2,DC3 -TargetCredential $cred -Path "C:\ProgramData\PowerShell\Tasks\tesk1_credential.clixml"
 
 			Saves the credentials stored in $cred on the computers DC1, DC2, DC3 for use by the SYSTEM account
+
+		.EXAMPLE
+			PS C:\> Save-Credential -ComputerName DC1,DC2,DC3 -TargetCredential $cred -AccessGMSA contoso\svcDCTask$ -Path "C:\ProgramData\PowerShell\Tasks\tesk1_credential.clixml"
+
+			Saves the credentials stored in $cred on the computers DC1, DC2, DC3 for use by the contoso\svcDCTask$ gMSA account
 	#>
-	[Diagnostics.CodeAnalysis.SuppressMessageAttribute("PSAvoidUsingConvertToSecureStringWithPlainText", "")]
-	[CmdletBinding()]
-	Param (
-		[Parameter(Mandatory = $true)]
-		[PSCredential]
-		$TargetCredential,
+    [Diagnostics.CodeAnalysis.SuppressMessageAttribute("PSAvoidUsingConvertToSecureStringWithPlainText", "")]
+    [CmdletBinding(DefaultParameterSetName = 'SYSTEM')]
+    Param (
+        [Parameter(Mandatory = $true)]
+        [PSCredential]
+        $TargetCredential,
 
-		[Parameter(Mandatory = $true)]
-		[string]
-		$Path,
+        [Parameter(Mandatory = $true)]
+        [string]
+        $Path,
 
-		[PSCredential]
-		$AccessAccount,
+        [Parameter(Mandatory = $true, ParameterSetName = 'Account')]
+        [PSCredential]
+        $AccessAccount,
 
-		[Parameter(ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
-		[string[]]
-		$ComputerName,
-		
-		[PSCredential]
-		$Credential
-	)
+        [Parameter(Mandatory = $true, ParameterSetName = 'GMSA')]
+        [string]
+        $AccessGMSA,
 
-	process
-	{
-		$parameters = @{
-			ArgumentList = $TargetCredential, $Path, $AccessAccount
-		}
-		if ($ComputerName) { $parameters.ComputerName = $ComputerName }
-		if ($Credential) { $parameters.Credential = $Credential }
-		
-		Invoke-Command @parameters -ScriptBlock {
-			Param (
-				[PSCredential]
-				$Credential,
+        [Parameter(ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
+        [string[]]
+        $ComputerName,
 
-				[string]
-				$Path,
+        [PSCredential]
+        $Credential
+    )
 
-				[PSCredential]
-				$AccessAccount
-			)
+    process {
+        $parameters = @{
+            ArgumentList = $TargetCredential, $Path, $AccessAccount, $AccessGMSA
+        }
+        if ($ComputerName) { $parameters.ComputerName = $ComputerName }
+        if ($Credential) { $parameters.Credential = $Credential }
 
-			#region Folder Management
-			if (Test-Path -Path $Path)
-			{
-				$item = Get-Item $Path
-				if ($item.PSIsContainer)
-				{
-					$folder = $item.FullName
-					$file = Join-Path $folder 'Credential.clixml'
-				}
-				else
-				{
-					$folder = Split-Path $item.FullName
-					$file = $item.FullName
-				}
-			}
-			else
-			{
-				if ([System.IO.Path]::GetExtension($Path))
-				{
-					$folder = Split-Path $Path
-					$file = $Path
-				}
-				else
-				{
-					$folder = $Path
-					$file = Join-Path $folder 'Credential.clixml'
-				}
-			}
-			if (-not (Test-Path -Path $folder))
-			{
-				$null = New-Item -Path $folder -ItemType Directory -Force -ErrorAction Stop
-			}
-			#endregion Folder Management
+        Invoke-Command @parameters -ScriptBlock {
+            Param (
+                [PSCredential]
+                $Credential,
 
-			#region Access Privileges
-			$accessUserName = $AccessAccount.UserName
-			if (-not $accessUserName) { $accessUserName = "SYSTEM" }
-			$acl = Get-Acl -Path $folder
-			if (-not ($acl.Access | Where-Object IdentityReference -like $accessUserName | Where-Object {
+                [string]
+                $Path,
+
+                [PSCredential]
+                $AccessAccount,
+
+                [AllowEmptyString()]
+                [string]
+                $AccessGMSA
+            )
+
+            #region Folder Management
+            if (Test-Path -Path $Path) {
+                $item = Get-Item $Path
+                if ($item.PSIsContainer) {
+                    $folder = $item.FullName
+                    $file = Join-Path $folder 'Credential.clixml'
+                }
+                else {
+                    $folder = Split-Path $item.FullName
+                    $file = $item.FullName
+                }
+            }
+            else {
+                if ([System.IO.Path]::GetExtension($Path)) {
+                    $folder = Split-Path $Path
+                    $file = $Path
+                }
+                else {
+                    $folder = $Path
+                    $file = Join-Path $folder 'Credential.clixml'
+                }
+            }
+            if (-not (Test-Path -Path $folder)) {
+                $null = New-Item -Path $folder -ItemType Directory -Force -ErrorAction Stop
+            }
+            #endregion Folder Management
+
+            #region Access Privileges
+            $accessUserName = $AccessAccount.UserName
+            if ($AccessGMSA) { $accessUserName = $AccessGMSA }
+            if (-not $accessUserName) { $accessUserName = "SYSTEM" }
+            $acl = Get-Acl -Path $folder
+            if (-not ($acl.Access | Where-Object IdentityReference -Like $accessUserName | Where-Object {
 				($_.FileSystemRights -Band 278) -and ($_.FileSystemRights -Band 65536)
-			}))
-			{
-				$rule = New-Object System.Security.AccessControl.FileSystemAccessRule($accessUserName, 'Read, Write', 'Allow')
-				$null = $acl.AddAccessRule($rule)
-				$acl | Set-Acl -Path $folder
-			}
-			#endregion Access Privileges
+                    })) {
+                $rule = New-Object System.Security.AccessControl.FileSystemAccessRule($accessUserName, 'Read, Write', 'Allow')
+                $null = $acl.AddAccessRule($rule)
+                $acl | Set-Acl -Path $folder
+            }
+            #endregion Access Privileges
 
-			#region Create Task
-			$folderCleaned = (Get-Item $folder).FullName
-			$credFile = "{0}\{1}.txt" -f $folderCleaned, ([guid]::NewGuid())
-			$task = {
-				$password = [System.IO.File]::ReadAllText("<credfile>")
-				Remove-Item -Path "<credfile>"
-				$credential = New-Object PSCredential("<username>", ($password | ConvertTo-SecureString -AsPlainText -Force))
-				$credential | Export-Clixml -Path "<exportPath>"
-			}
-			$commandString = $task.ToString().Replace("<credfile>", $credFile).Replace("<username>", $Credential.UserName).Replace("<exportPath>", $file)
-			$encodedCommand = [convert]::ToBase64String(([System.Text.Encoding]::Unicode.GetBytes($commandString)))
+            #region Create Task
+            $folderCleaned = (Get-Item $folder).FullName
+            $credFile = "{0}\{1}.txt" -f $folderCleaned, ([guid]::NewGuid())
+            $task = {
+                $password = [System.IO.File]::ReadAllText("<credfile>")
+                Remove-Item -Path "<credfile>"
+                $credential = New-Object PSCredential("<username>", ($password | ConvertTo-SecureString -AsPlainText -Force))
+                $credential | Export-Clixml -Path "<exportPath>"
+            }
+            $commandString = $task.ToString().Replace("<credfile>", $credFile).Replace("<username>", $Credential.UserName).Replace("<exportPath>", $file)
+            $encodedCommand = [convert]::ToBase64String(([System.Text.Encoding]::Unicode.GetBytes($commandString)))
 
-			$action = New-ScheduledTaskAction -Execute powershell.exe -Argument "-NoProfile -EncodedCommand $encodedCommand"
-			
-			if ($accessUserName -ne "SYSTEM") { $principal = New-ScheduledTaskPrincipal -UserId $accessUserName -LogonType Interactive }
-			else { $principal = New-ScheduledTaskPrincipal -UserId 'SYSTEM' -LogonType Interactive }
-			$taskItem = New-ScheduledTask -Action $action -Principal $principal -Description "Temporary Task"
-			$parametersRegister = @{
-				TaskName = "TempTask_$([guid]::NewGuid())"
-				InputObject = $taskItem
-			}
-			if ($accessUserName -ne "SYSTEM")
-			{
-				$parametersRegister["User"] = $AccessAccount.UserName
-				$parametersRegister["Password"] = $AccessAccount.GetNetworkCredential().Password
-			}
-			$null = Register-ScheduledTask @parametersRegister
-			#endregion Create Task
+            $action = New-ScheduledTaskAction -Execute powershell.exe -Argument "-NoProfile -EncodedCommand $encodedCommand"
 
-			#region Perform Encryption
-			[System.IO.File]::WriteAllText($credFile, $Credential.GetNetworkCredential().Password)
-			Start-ScheduledTask -TaskName $parametersRegister.TaskName
-			Start-Sleep -Seconds 5
-			#endregion Perform Encryption
+            if ($accessUserName -eq "SYSTEM") { $principal = New-ScheduledTaskPrincipal -UserId $accessUserName -LogonType Interactive }
+            elseif ($AccessGMSA) { $principal = New-ScheduledTaskPrincipal -UserId $AccessGMSA -LogonType Password }
+            else { $principal = New-ScheduledTaskPrincipal -UserId $accessUserName -LogonType Password }
+            $taskItem = New-ScheduledTask -Action $action -Principal $principal -Description "Temporary Task"
+            $parametersRegister = @{
+                TaskName    = "TempTask_$([guid]::NewGuid())"
+                InputObject = $taskItem
+            }
+            if ($accessUserName -ne "SYSTEM" -and -not $AccessGMSA) {
+                $parametersRegister["User"] = $AccessAccount.UserName
+                $parametersRegister["Password"] = $AccessAccount.GetNetworkCredential().Password
+            }
+            $null = Register-ScheduledTask @parametersRegister
+            #endregion Create Task
 
-			#region Cleanup
-			Unregister-ScheduledTask -TaskName $parametersRegister.TaskName -Confirm:$false
-			if (Test-Path -Path $credFile)
-			{
-				try { Remove-Item $credFile -Force -ErrorAction Stop }
-				catch
-				{
-					Write-Warning "[$env:COMPUTERNAME] Clear Text Credential File still exists!! $credFile | $_"
-				}
-			}
-			if (-not (Test-Path -Path $file))
-			{
-				throw "[$env:COMPUTERNAME] Failed to create credential file! ($file)"
-			}
-			#endregion Cleanup
-		}
-	}
+            #region Perform Encryption
+            [System.IO.File]::WriteAllText($credFile, $Credential.GetNetworkCredential().Password)
+            Start-ScheduledTask -TaskName $parametersRegister.TaskName
+            Start-Sleep -Seconds 5
+            #endregion Perform Encryption
+
+            #region Cleanup
+            Unregister-ScheduledTask -TaskName $parametersRegister.TaskName -Confirm:$false
+            if (Test-Path -Path $credFile) {
+                try { Remove-Item $credFile -Force -ErrorAction Stop }
+                catch {
+                    Write-Warning "[$env:COMPUTERNAME] Clear Text Credential File still exists!! $credFile | $_"
+                }
+            }
+            if (-not (Test-Path -Path $file)) {
+                throw "[$env:COMPUTERNAME] Failed to create credential file! ($file)"
+            }
+            #endregion Cleanup
+        }
+    }
 }
