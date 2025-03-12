e3672c7a148ee2f55962590db0ca5675e5895688
Friedrich Weinmann <FriedrichWeinmann@users.noreply.github.com>
2025-03-12 17:56:54 +0100

    updates

---
+++ experiments/sql/datatables-copy-filter.ps1
@@ -0,0 +1,45 @@
+﻿<#
+A quick experiment on comparing and filtering datasets between DataTables, then build a new DataTable from it.
+
+Invoke-Sqlcmd can return full DataTable objects.
+Write-SqlDataTable can write a DataTable into a table.
+
+This allows transfering selected datasets from one SqlInstance to another.
+#>
+
+$table1 = [System.Data.DataTable]::new()
+$table2 = [System.Data.DataTable]::new()
+
+$columns = @(
+	@{ Name = "Name"; Type = [string] }
+	@{ Name = "Description"; Type = [string] }
+	@{ Name = "Age"; Type = [int] }
+)
+foreach ($column in $columns) {
+	$null = $table1.Columns.Add($column.Name, $column.Type)
+	$null = $table2.Columns.Add($column.Name, $column.Type)
+}
+
+$entries1 = @(
+	@{ Name = "Fred"; Description = "Whatever 1"; Age = 39 }
+	@{ Name = "Max"; Description = "Whatever 2"; Age = 25 }
+	@{ Name = "Peter"; Description = "Whatever 3"; Age = 27 }
+	@{ Name = "Maria"; Description = "Whatever 4"; Age = 18 }
+)
+foreach ($entry in $entries1) {
+	$null = $table1.Rows.Add($entry.Name, $entry.Description, $entry.Age)
+}
+
+$entries2 = @(
+	@{ Name = "Fred"; Description = "Whatever 1"; Age = 39 }
+	@{ Name = "Max"; Description = "Whatever 2"; Age = 27 }
+	@{ Name = "Moritz"; Description = "Whatever 3"; Age = 27 }
+)
+foreach ($entry in $entries2) {
+	$null = $table2.Rows.Add($entry.Name, $entry.Description, $entry.Age)
+}
+
+$entries = @($table1.Rows).Where{ -not $table2.Select("Name='$($_["Name"])' and Age=$($_["Age"])") }
+$newTable = [System.Data.DataTable]::new()
+foreach ($column in $table1.Columns) { $null = $newTable.Columns.Add($column.Name, $column.DataType) }
+@($entries).ForEach{ $null = $newTable.Rows.Add($_.ItemArray) }
--- readme.md
+++ readme.md
@@ -1,3 +1,9 @@
 ﻿# Fred's Snippets
 
-This is my dumping ground for unrelated commands or scripts I wrote at some point and haven't added to a module yet.
+This is my dumping ground for unrelated commands or scripts I wrote at some point and (probably) haven't added to a module yet.
+
+What can you find here?
+
++ Functions: Mostly standalone functions, ready to plug into your scripts.
++ Scripts: Finished scripts for a dedicated purpose, ready to deploy/schedule/whatever.
++ Experiments: Random bits of PowerShell, as experiments on making stuff work. Not directly useful, but might be something to learn from it.
